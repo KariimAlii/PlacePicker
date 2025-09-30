3b0a05c35b390793538c2c791dc90977d4858ea5
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-30 23:47:25 +0300

    Use Case for useEffect (2) : We need to use useEffect() Hook

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,6 +4,15 @@ import { createPortal } from 'react-dom';
 const Modal = function Modal({ open, children }) {
   const dialog = useRef();
 
+  // now (dialog.current) is undefined
+  // so that the first time this component gets executed ,
+  // the ref to the dialog will not be set yet
+
+  // in this case => you need to use useEffect()
+  // because you need to manipulate the DOM
+  // and useEffect() will be executed after the component function gets executed
+  open ? dialog.current.showModal() : dialog.current.close();
+
   return createPortal(
     <dialog className="modal" ref={dialog} open={open}>
       {children}
