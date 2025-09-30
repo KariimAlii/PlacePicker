9bc1fba3d9efe6b06ac03bc61038088616317bb8
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-09-30 23:51:17 +0300

    Use Case for useEffect (2) : UseEffect() that has dependencies

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,20 +1,18 @@
-import { useRef } from 'react';
+import {useEffect, useRef} from 'react';
 import { createPortal } from 'react-dom';
 
 const Modal = function Modal({ open, children }) {
   const dialog = useRef();
 
-  // now (dialog.current) is undefined
-  // so that the first time this component gets executed ,
-  // the ref to the dialog will not be set yet
-
-  // in this case => you need to use useEffect()
-  // because you need to manipulate the DOM
-  // and useEffect() will be executed after the component function gets executed
-  open ? dialog.current.showModal() : dialog.current.close();
+  useEffect(() => {
+    // in this case => you need to use useEffect()
+    // because you need to manipulate the DOM
+    // and useEffect() will be executed after the component function gets executed
+    open ? dialog.current.showModal() : dialog.current.close();
+  }, [open]) // having an empty array of dependencies will give you a warning !!
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog}>
       {children}
     </dialog>,
     document.getElementById('modal')
