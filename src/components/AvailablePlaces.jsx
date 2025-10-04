56b46cb6aefa91d05438525a3e311490944ee232
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 16:14:46 +0300

    Handling Errors & Error Page

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,18 +1,30 @@
 ﻿import Places from "./Places.jsx";
 import {useEffect, useState} from "react";
+import ErrorPage from "./ErrorPage.jsx";
 
 //! using async is not allowed for component functions
 export default function AvailablePlaces({ onSelectPlace }) {
+    //! Fetching Data : Data State , Loading State , Error State
     const [availablePlaces, setAvailablePlaces] = useState([])
     const [isFetching, setIsFetching] = useState([])
+    const [error, setError] = useState()
     //! Available Places needs to be fetched from backend
     //! This Effect will be executed once after the execution of component function
     useEffect(() => {
         async function fetchPlaces() {
             setIsFetching(true);
-            const response = await fetch('http://localhost:3000/places');
-            const data = await response.json();
-            setAvailablePlaces(data.places);
+            try {
+                const response = await fetch('http://localhost:3000/placess');
+                const data = await response.json();
+                if(!response.ok) {
+                    //! ok : success status code 200 , 300
+                    //! otherwise : failure code 400 , 500
+                    throw new Error('failed to fetch places')
+                }
+                setAvailablePlaces(data.places);
+            } catch (err) {
+                setError(err);
+            }
             setIsFetching(false);
         }
 
@@ -20,7 +32,14 @@ export default function AvailablePlaces({ onSelectPlace }) {
     }, [])
 
 
-
+    if(error) {
+        return (
+            <ErrorPage
+                title="An error occured!.."
+                message={error.message}
+            />
+        )
+    }
     return (
         <Places
             title="Available Places"
