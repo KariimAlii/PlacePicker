30d791ba53cc753269bf841403480472fe7393b9
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 15:25:39 +0300

    Added Available Places Component

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,13 @@
+﻿import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+    //! Available Places needs to be fetched from backend
+    return (
+        <Places
+            title="Available Places"
+            places={[]}
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    )
+}
