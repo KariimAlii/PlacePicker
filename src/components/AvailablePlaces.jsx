18c51412bbcb6899950f2f307da8f7bde0f919b3
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 15:34:26 +0300

    Using Fetch : async is not allowed for component functions

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,9 +1,13 @@
 ﻿import Places from "./Places.jsx";
 import {useState} from "react";
 
-export default function AvailablePlaces({ onSelectPlace }) {
+//! using async is not allowed for component functions
+export default async function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([])
     //! Available Places needs to be fetched from backend
+    const response = await fetch('http://localhost:3000/places')
+
+
     return (
         <Places
             title="Available Places"
