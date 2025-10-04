9b03df6935a0afbcb351b5402a108d129891636c
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 15:28:56 +0300

    What we did previously using local storage

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,11 +1,15 @@
 ﻿import Places from "./Places.jsx";
+import {useState} from "react";
+
+const places = localStorage.getItem("places");
 
 export default function AvailablePlaces({ onSelectPlace }) {
+    const [availablePlaces, setAvailablePlaces] = useState(places)
     //! Available Places needs to be fetched from backend
     return (
         <Places
             title="Available Places"
-            places={[]}
+            places={availablePlaces}
             fallbackText="No places available."
             onSelectPlace={onSelectPlace}
         />
