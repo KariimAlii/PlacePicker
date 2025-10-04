5b303119e937c43aab5540acf9e9fe8df51677e5
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-04 15:45:19 +0300

    How to use async/await in an Effect callback

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -6,9 +6,14 @@ export default function AvailablePlaces({ onSelectPlace }) {
     const [availablePlaces, setAvailablePlaces] = useState([])
     //! Available Places needs to be fetched from backend
     //! This Effect will be executed once after the execution of component function
-    useEffect(async () => {
-        await fetch('http://localhost:3000/places')
+    useEffect(() => {
+        async function fetchPlaces() {
+            const response = await fetch('http://localhost:3000/places');
+            const data = await response.json();
+            setAvailablePlaces(data.places);
+        }
 
+        fetchPlaces();
     }, [])
 
 
