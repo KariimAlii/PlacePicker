22935d7afd487fedccb6626a862d9d36b4cdc38b
Karim Ali <101140331+KariimAlii@users.noreply.github.com>
2025-10-08 02:37:47 +0300

    Create Custom React Hook (1)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import ErrorPage from "./ErrorPage.jsx";
 import {sortPlacesByDistance} from "../loc.js";
 import {fetchAvailablePlaces} from "../proxies.js";
+import useFetch from "../hooks/useFetch.js";
 
 //! using async is not allowed for component functions
 export default function AvailablePlaces({ onSelectPlace }) {
@@ -12,37 +13,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     const [error, setError] = useState()
     //! Available Places needs to be fetched from backend
     //! This Effect will be executed once after the execution of component function
-    useEffect(() => {
-        async function fetchPlaces() {
-            //! const [availablePlaces, setAvailablePlaces] = useState([])
-            //! ESLint: React Hook "useState" is called in function "fetchPlaces"
-            //! that is neither a React function component nor a custom React Hook function.
-            //! React component names must start with an uppercase letter.
-            //! React Hook names must start with the word "use".(react-hooks/ rules-of-hooks
-            setIsFetching(true);
-            try {
-                const places = await fetchAvailablePlaces();
 
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        const sortedPlaces = sortPlacesByDistance(
-                            places,
-                            position.coords.latitude,
-                            position.coords.longitude
-                        );
-
-                        setAvailablePlaces(sortedPlaces);
-                        setIsFetching(false);
-                    }
-                );
-            } catch (err) {
-                setError(err);
-                setIsFetching(false);
-            }
-        }
-
-        fetchPlaces();
-    }, [])
+    useFetch()
 
 
     if(error) {
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,40 @@
+﻿//! React Rule => Functions starting with (use) are hooks
+//! You need to name the function starting with (use)
+//!
+//! Any React Hook must be called
+//! --- (1) at the top level of React function component
+//! --- (2) at a custom React Hook function
+//! It can't be nested inside if condition or callback ... etc
+import {useEffect} from "react";
+import {fetchAvailablePlaces} from "../proxies.js";
+import {sortPlacesByDistance} from "../loc.js";
+
+export default function useFetch() {
+    //! You can use other hooks inside your custom hook
+    useEffect(() => {
+        async function fetchPlaces() {
+            setIsFetching(true);
+            try {
+                const places = await fetchAvailablePlaces();
+
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const sortedPlaces = sortPlacesByDistance(
+                            places,
+                            position.coords.latitude,
+                            position.coords.longitude
+                        );
+
+                        setAvailablePlaces(sortedPlaces);
+                        setIsFetching(false);
+                    }
+                );
+            } catch (err) {
+                setError(err);
+                setIsFetching(false);
+            }
+        }
+
+        fetchPlaces();
+    }, [])
+}
